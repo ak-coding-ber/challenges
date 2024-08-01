dca4ccc3df18146e39ee26f8de88d568c7e5d121
andreask <a.kupka1@gmx.net>
2024-08-01 12:57:34 +0200

    solution js-savings-tracker challenge

---
+++ js-variables-and-numbers/js-variables-and-numbers_track_savings/js/index.js
@@ -0,0 +1,12 @@
+const startingBalance = 1000;
+const monthlyContribution = 200;
+const months = 12;
+const totalSavings = startingBalance + monthlyContribution * months;
+
+console.log(
+  "After",
+  months,
+  "months of saving, you will have",
+  totalSavings,
+  "€ in your account."
+);
