a53f2a3bcd73ef8d732b087f52c79c6629748ebd
andreask <a.kupka1@gmx.net>
2024-08-02 13:03:58 +0200

    js-basics-dark-mode solution

--- js-basics/js-basics_dark-mode/js/index.js
+++ js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,29 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+// Define behaviour when Dark Mode button is clicked
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  console.log("Dark Mode Button was clicked.");
+  bodyElement.classList.add("dark");
+});
+
+// Define behaviour when Light Mode button is clicked
+
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+
+lightModeButton.addEventListener("click", () => {
+  console.log("Light Mode Button was clicked.");
+  bodyElement.classList.remove("dark");
+});
+
+// Define behaviour when Toggle Button is clicked
+
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+toggleButton.addEventListener("click", () => {
+  console.log("Toggle Button was clicked.");
+  bodyElement.classList.toggle("dark");
+});
