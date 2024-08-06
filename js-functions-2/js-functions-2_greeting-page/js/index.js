663e65267821f5697f2b41e61c4c29f1e5ecb53d
andreask <a.kupka1@gmx.net>
2024-08-06 12:43:36 +0200

    js-functions-2_greeting-page solution

--- js-functions-2/js-functions-2_greeting-page/js/index.js
+++ js-functions-2/js-functions-2_greeting-page/js/index.js
@@ -24,10 +24,32 @@ const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
   // Code here
+  const hour = new Date().getHours();
+  if (hour >= 6 && hour <= 12) {
+    return "Good Morning";
+  } else if (hour >= 13 && hour <= 18) {
+    return "Good Afternoon";
+  } else if (hour >= 19 && hour <= 22) {
+    return "Good Evening";
+  } else if (hour <= 5 || hour >= 22) {
+    return "Good Night";
+  } else {
+    return "Error";
+  }
 }
 
 function getDayColor() {
   // Code here
+  const weekday = new Date().getDay();
+  if (weekday === 1) {
+    return "darkgrey";
+  } else if (weekday >= 2 && weekday <= 5) {
+    return "lightblue";
+  } else if (weekday == 6 || 7) {
+    return "hotpink";
+  } else {
+    return "Error";
+  }
 }
 
 display.textContent = getGreeting();
