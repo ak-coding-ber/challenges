d6f37527d476bb4d5c44d9d1fdec010b41bf4b0b
andreask <a.kupka1@gmx.net>
2024-08-07 18:19:07 +0200

    js-forms_checkbox-input solution

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,28 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+hideTosError();
+successMessage.style.display = "none";
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const formElements = event.target.elements;
+  const tosChecked = formElements.tos.checked;
 
+  if (tosChecked) {
+    hideTosError();
+    successMessage.style.display = "block";
+    event.target.reset();
+  } else {
+    showTosError();
+    successMessage.style.display = "none";
+    return;
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  // I disabled the alert, because successMessage is not shown until you click 'okay' on the warning window
+  // alert("Form submitted");
 });
