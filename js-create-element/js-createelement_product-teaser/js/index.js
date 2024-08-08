dca8380b928258a64be1866e0de8663af1f06297
andreask <a.kupka1@gmx.net>
2024-08-08 16:04:33 +0200

    js-createelement_product-teaser solution

--- js-create-element/js-createelement_product-teaser/js/index.js
+++ js-create-element/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,41 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const productCard = document.createElement("article");
+productCard.classList.add("product");
+
+// creating html-structure for second product-card
+productCard.innerHTML = `
+  <div class="product__body">
+    <div class="product__text-container">
+      <h2 class="product__name">${name}</h2>
+      <ul class="product__categories">
+        <li class="product__category">${category1}</li>
+        <li class="product__category">${category2}</li>
+        <li class="product__category">${category3}</li>
+      </ul>
+      <p class="product__description">
+        ${description}
+      </p>
+    </div>
+    <div class="product__image-container">
+      <img
+        class="product__image"
+        src=${imageSrc}
+        alt=""
+      />
+    </div>
+  </div>
+  <footer class="product__footer">
+    <span class="product__price">${price} €</span>
+    <button type="button" class="product__buy-button">Buy</button>
+  </footer>
+`;
+
+document.body.append(productCard);
+
+// adding event listener for Buy-button
+productCard.addEventListener("click", () => {
+  console.log(`Product Name: ${name}\nPrice: ${price}`);
+});
