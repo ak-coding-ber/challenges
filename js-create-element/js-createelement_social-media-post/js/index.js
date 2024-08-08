a2cdae121f0befa901c6e288bcd75f32c2a4b1a4
andreask <a.kupka1@gmx.net>
2024-08-08 14:10:14 +0200

    js-createelement_social-media-post solution

--- js-create-element/js-createelement_social-media-post/js/index.js
+++ js-create-element/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,28 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+newPostSection = document.createElement("section");
+newPostSection.classList.add("post");
+
+newPostText = document.createElement("p");
+newPostText.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+newPostText.classList.add("post__content");
+
+newPostFooter = document.createElement("footer");
+newPostFooter.classList.add("post__footer");
+
+newPostSection.append(newPostText, newPostFooter);
+
+newPostCreatedByInfo = document.createElement("span");
+newPostCreatedByInfo.classList.add("post__username");
+newPostCreatedByInfo.textContent = "@username";
+
+newPostLikeButton = document.createElement("button");
+newPostLikeButton.classList.add("post__button");
+newPostLikeButton.textContent = "♥ Like";
+newPostLikeButton.addEventListener("click", handleLikeButtonClick);
+
+newPostFooter.append(newPostCreatedByInfo, newPostLikeButton);
+
+document.body.append(newPostSection);
