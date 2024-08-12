fed669005442db9287cc098651c4e570c2324cfe
andreask <a.kupka1@gmx.net>
2024-08-12 16:21:30 +0200

    js-loops_stars solution

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,24 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(numberOfStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  let filledStars = numberOfStars;
 
+  for (i = 1; i <= 5; i++) {
+    const img = document.createElement("img");
+    if (i <= filledStars) {
+      img.setAttribute("src", "./assets/star-filled.svg");
+    } else {
+      img.setAttribute("src", "./assets/star-empty.svg");
+    }
+
+    starContainer.append(img);
+  }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(3);
