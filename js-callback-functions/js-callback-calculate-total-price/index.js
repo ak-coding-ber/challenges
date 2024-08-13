adce40f18f3adb11367da62d7be8e6f4d6a090ac
andreask <a.kupka1@gmx.net>
2024-08-13 12:26:59 +0200

    js-callback-calculate-total-price solution

---
+++ js-callback-functions/js-callback-calculate-total-price/index.js
@@ -0,0 +1,25 @@
+const products = [
+  { name: "Saffron", price: 50 },
+  { name: "Cinnamon", price: 6 },
+  { name: "Cardamom", price: 12 },
+  { name: "Turmeric", price: 9 },
+  { name: "Paprika", price: 4 },
+  { name: "Garam Masala", price: 10 },
+  { name: "Vanilla Bean", price: 15 },
+  { name: "Coriander", price: 6 },
+];
+
+function extractPrice(product) {
+  const productPrice = product.price;
+  return productPrice;
+}
+
+function calculateTotalPrice(products, extractPrice) {
+  let totalPrice = 0;
+  for (product of products) {
+    totalPrice += extractPrice(product);
+  }
+  console.log(`The total price is: ${totalPrice}`);
+}
+
+calculateTotalPrice(products, extractPrice);
