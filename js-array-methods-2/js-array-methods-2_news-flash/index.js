246a14a9e46cc257005cdcf4fefef95ee9f50497
andreask <a.kupka1@gmx.net>
2024-08-15 13:41:37 +0200

    js-array-methods-2_news-flash solution

--- js-array-methods-2/js-array-methods-2_news-flash/index.js
+++ js-array-methods-2/js-array-methods-2_news-flash/index.js
@@ -5,12 +5,14 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+const filteredNews = news.filter((entry) =>
+  entry.categories.includes("politics")
+);
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews
+  .slice()
+  .sort((a, b) => a.body.length - b.body.length);
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
