c80a7031e9aed58c82f146456ed07b9ebe31c448
andreask <a.kupka1@gmx.net>
2024-08-19 08:34:42 +0200

    upload js-fetch challenges initial state

---
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,7 @@
+console.clear();
+
+const url = "https://swapi.py4e.com/api/people";
+
+function fetchData() {}
+
+fetchData();
---
+++ js-fetch/js-fetch_star-wars/js/index.js
@@ -0,0 +1,46 @@
+import { Card } from "../components/Card/Card.js";
+import { renderElement } from "./utils.js";
+
+console.clear();
+
+const EXAMPLE_DATA = {
+  name: "Luke Skywalker",
+  height: "172",
+  mass: "77",
+  hairColor: "blond",
+  skin_color: "fair",
+  eye_color: "blue",
+  birth_year: "19BBY",
+  gender: "male",
+  homeworld: "hhttps://swapi.py4e.com/api/planets/1/",
+  films: [
+    "https://swapi.py4e.com/api/films/1/",
+    "https://swapi.py4e.com/apifilms/2/",
+    "https://swapi.py4e.com/api/films/3/",
+    "https://swapi.py4e.com/api/films/6/",
+  ],
+  species: [],
+  vehicles: [
+    "https://swapi.py4e.com/api/vehicles/14/",
+    "https://swapi.py4e.com/api/vehicles/30/",
+  ],
+  starships: [
+    "https://swapi.py4e.com/api/starships/12/",
+    "https://swapi.py4e.com/api/starships/22/",
+  ],
+  created: "2014-12-09T13:50:51.644000Z",
+  edited: "2014-12-20T21:17:56.891000Z",
+  url: "https://swapi.py4e.com/api/people/1/",
+};
+
+// Create dom element for a card and append it to the root
+const firstCard = Card(EXAMPLE_DATA);
+renderElement(firstCard);
+
+fetchDataAndRender();
+
+// --v-- your code below this line --v--
+
+function fetchDataAndRender() {
+  fetch(); // ?
+}
