38b47298c18cbac48c36ca870eaaf91a671c5323
andreask <a.kupka1@gmx.net>
2024-08-23 15:53:14 +0200

    react-basics_hello-world-article solution

--- react/react-basics/react-basics_hello-world-article/src/App.jsx
+++ react/react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Example Heading</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi sed ut
+        nobis praesentium delectus. Blanditiis deleniti perspiciatis suscipit
+        iste, esse necessitatibus earum veniam porro magni eius eum architecto
+        et provident?
+      </p>
+    </article>
+  );
 }
