9ede5feb395e7fc64945c5c3fbe1b5d9d81877b9
andreask <a.kupka1@gmx.net>
2024-08-23 15:13:30 +0200

    upload react-basics challenges initial files

---
+++ react/react-basics/react-basics_attributes/src/App.jsx
@@ -0,0 +1,5 @@
+import "./App.css";
+
+export default function App() {
+  return <div>Replace me with an article</div>;
+}
---
+++ react/react-basics/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,5 @@
+import "./App.css";
+
+export default function App() {
+  return <div>Replace me with your Component!</div>;
+}
