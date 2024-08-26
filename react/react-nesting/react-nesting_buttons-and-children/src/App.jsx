2cf28740d82b119c81dc023bf7fa0f02c6b77635
andreask <a.kupka1@gmx.net>
2024-08-26 15:28:01 +0200

    upload react-nesting challenges initial state

---
+++ react/react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <main>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </main>
+  );
+}
+
+function Button() {
+  return (
+    <button className="button" type="button">
+      Click me!
+    </button>
+  );
+}
---
+++ react/react-nesting/react-nesting_fragments/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Box from "./components/Box";
+
+export default function App() {
+  return (
+    <main className="flex-container">
+      <Box color="#007bff" />
+      <Box color="#fc3" />
+      <Box color="#ff3333" />
+    </main>
+  );
+}
