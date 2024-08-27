b0984e9aa32db8ddddb9b2ebd1e752613b689ca6
andreask <a.kupka1@gmx.net>
2024-08-27 16:36:28 +0200

    react-state_counter solution

--- react/react-state-1/react-state_counter/src/App.jsx
+++ react/react-state-1/react-state_counter/src/App.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
@@ -11,6 +12,7 @@ export default function App() {
           type="button"
           onClick={() => {
             console.log("🤔");
+            setCount(count - 1);
           }}
         >
           -
@@ -19,6 +21,7 @@ export default function App() {
           type="button"
           onClick={() => {
             console.log("🤔");
+            setCount(count + 1);
           }}
         >
           +
