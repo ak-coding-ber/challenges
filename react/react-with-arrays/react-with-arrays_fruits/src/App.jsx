00222f8376af4cce6ef68de6cdc8eec2ec360466
andreask <a.kupka1@gmx.net>
2024-08-28 09:30:11 +0200

    upload challenges react-with-arrays initial state

---
+++ react/react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card";
+import "./App.css";
+
+
+export default function App() {
+  const fruits = [];
+
+  return (
+    <div className="app">
+      <Card name="🍌 banana" />
+    </div>
+  );
+}
