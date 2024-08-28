c3d50a5bde4f743f34b59c6f47599b92c7077e78
andreask <a.kupka1@gmx.net>
2024-08-28 16:28:18 +0200

    react-state-2_table-reservation solution

--- react/react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react/react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,12 +1,23 @@
 import Counter from "./components/Counter";
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleAdd() {
+    setPeople(people + 1);
+  }
+
+  function handleSubtract() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onAdd={handleAdd} onSubtract={handleSubtract} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
--- react/react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
+++ react/react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "./Counter.css";
 
-
-export default function Counter() {
-  const [people, setPeople] = useState(0);
+export default function Counter({ onAdd, onSubtract }) {
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
@@ -12,7 +9,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
+          onClick={onAdd}
         >
           +
         </button>
@@ -20,7 +17,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
+          onClick={onSubtract}
         >
           -
         </button>
