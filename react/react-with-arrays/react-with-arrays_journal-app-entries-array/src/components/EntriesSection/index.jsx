5decd17f3103edc31c65959e326b7c51c9216928
andreask <a.kupka1@gmx.net>
2024-08-28 15:04:42 +0200

    react-with-arrays_journal-app-entries-array solution

--- react/react-with-arrays/react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.jsx
+++ react/react-with-arrays/react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.jsx
@@ -5,6 +5,36 @@ import Tabs from "../Tabs";
 import Tab from "../Tab";
 import Badge from "../Badge";
 
+const entries = [
+  {
+    id: 1000,
+    date: "Feb 5, 2025",
+    motto: "We are in a state of chaos",
+    notes:
+      "Today I learned about React State. It was fun! I can't wait to learn more.",
+  },
+  {
+    id: 999,
+    date: "Feb 4, 2025",
+    motto: "Props, Props, Props",
+    notes:
+      "Today I learned about React Props. Mad props to everyone who understands this!",
+  },
+  {
+    id: 998,
+    date: "Feb 3, 2025",
+    motto: "How to nest components online fast",
+    notes:
+      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+  },
+  {
+    id: 997,
+    date: "Feb 2, 2025",
+    motto: "I'm a React Developer",
+    notes: "My React-ion when I learned about React: 😍",
+  },
+];
+
 export default function EntriesSection() {
   return (
     <section className="entries-section">
@@ -17,23 +47,19 @@ export default function EntriesSection() {
         </Tab>
       </Tabs>
       <div className="entries-section__entries">
-        <Entry
-          date="Feb 27, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
-        <Divider />
-        <Entry
-          date="Feb 26, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
-        <Divider />
-        <Entry
-          date="Feb 25, 2028"
-          motto="Thats life in the city"
-          notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
+        {entries.map((entry) => {
+          return (
+            <>
+              {entries.indexOf(entry) === 0 ? undefined : <Divider />}
+              <Entry
+                date={entry.date}
+                motto={entry.motto}
+                notes={entry.notes}
+                key={entry.id}
+              />
+            </>
+          );
+        })}
       </div>
     </section>
   );
