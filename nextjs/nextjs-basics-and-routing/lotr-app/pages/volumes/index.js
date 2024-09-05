0614f2461fd13907fa9e9d5ef96cee11d866158c
andreask <a.kupka1@gmx.net>
2024-09-05 08:40:28 +0200

    added missing introduction text

--- nextjs/nextjs-basics-and-routing/lotr-app/pages/volumes/index.js
+++ nextjs/nextjs-basics-and-routing/lotr-app/pages/volumes/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { introduction } from "@/lib/data";
 
 export default function Volumes() {
   return (
     <>
       <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
       <h2>All Volumes</h2>
       <ul>
         <li>
