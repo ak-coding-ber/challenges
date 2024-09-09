f315dbf03d0905b2d57c1e882d42799beef8079b
ak-coding-ber <a.kupka1@gmx.net>
2024-09-09 12:09:39 +0200

    react-immutable-state_form solution

--- react/react-immutable-state/react-immutable-state_form/components/Form/index.js
+++ react/react-immutable-state/react-immutable-state_form/components/Form/index.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { StyledForm, StyledInputContainer } from "./Form.styled";
+import { useImmer } from "use-immer";
 
 export default function Form() {
-  const [mountain, setMountain] = useState({
+  const [mountain, updateMountain] = useImmer({
     name: "Mount Everest",
     values: {
       altitude: 8848,
@@ -10,11 +11,51 @@ export default function Form() {
     },
   });
 
-  function handleNameChange(event) {}
+  function handleNameChange(event) {
+    updateMountain((draft) => {
+      draft.name = event.target.value;
+    });
+  }
 
-  function handleAltitudeChange(event) {}
+  function handleAltitudeChange(event) {
+    updateMountain((draft) => {
+      draft.values.altitude = event.target.value;
+    });
+  }
 
-  function handleMountainRangeChange(event) {}
+  function handleMountainRangeChange(event) {
+    updateMountain((draft) => {
+      draft.values.mountainRange = event.target.value;
+    });
+  }
+
+  // alternative approach
+
+  // const [mountain, setMountain] = useState({
+  //   name: "Mount Everest",
+  //   values: {
+  //     altitude: 8848,
+  //     mountainRange: "Himalayas",
+  //   },
+  // });
+
+  // function handleNameChange(event) {
+  //   setMountain({ ...mountain, name: event.target.value });
+  // }
+
+  // function handleAltitudeChange(event) {
+  //   setMountain({
+  //     ...mountain,
+  //     values: { ...mountain.values, altitude: event.target.value },
+  //   });
+  // }
+
+  // function handleMountainRangeChange(event) {
+  //   setMountain({
+  //     ...mountain,
+  //     values: { ...mountain.values, mountainRange: event.target.value },
+  //   });
+  // }
 
   return (
     <StyledForm>
