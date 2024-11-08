c5acdb9595f2de82b1aeb0645d44af255ce75307
ak-coding-ber <a.kupka1@gmx.net>
2024-11-08 10:59:27 +0100

    added header according to figma design - erased unnessary files

---
+++ react/react-project-setup/journal/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.headerH1}>JOURNAL</h1>
+    </div>
+  );
+}
