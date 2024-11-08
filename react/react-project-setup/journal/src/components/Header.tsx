ea3bd25faad0d4e99135da21748706eff041de40
ak-coding-ber <a.kupka1@gmx.net>
2024-11-08 13:18:28 +0100

    refactored header component

--- react/react-project-setup/journal/src/components/Header.tsx
+++ react/react-project-setup/journal/src/components/Header.tsx
@@ -2,8 +2,8 @@ import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <div className={styles.header}>
-      <h1 className={styles.headerH1}>JOURNAL</h1>
-    </div>
+    <header className={styles.header}>
+      <h1>JOURNAL</h1>
+    </header>
   );
 }
