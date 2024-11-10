24a6ae72378d8e6f576ef8598055497b633ae6ce
ak-coding-ber <a.kupka1@gmx.net>
2024-11-10 12:43:01 +0100

    fixed inconsistency in bem naming

--- react/react-project-setup/journal/src/components/Content.tsx
+++ react/react-project-setup/journal/src/components/Content.tsx
@@ -1,11 +1,12 @@
 import NewEntryForm from "./NewEntryForm";
 import EntriesAndTabs from "./EntriesAndTabs";
+import "./Content.css";
 
 export default function Content() {
   return (
-    <>
+    <main className="content">
       <NewEntryForm />
       <EntriesAndTabs />
-    </>
+    </main>
   );
 }
--- react/react-project-setup/journal/src/components/CreateNewEntryButton.tsx
+++ react/react-project-setup/journal/src/components/CreateNewEntryButton.tsx
@@ -2,7 +2,7 @@ import "./CreateNewEntryButton.css";
 
 export default function CreateNewEntryButton() {
   return (
-    <button type="submit" className="button__createNewEntry">
+    <button type="submit" className="newEntryForm__button--create">
       Create
     </button>
   );
