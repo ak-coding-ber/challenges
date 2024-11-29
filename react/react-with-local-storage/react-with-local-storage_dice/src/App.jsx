b78e13db9b9e645c2735b37b112abeefa736867d
ak-coding-ber <a.kupka1@gmx.net>
2024-11-29 11:03:11 +0100

    made rolls history persistent via local-storage-state

--- react/react-with-local-storage/react-with-local-storage_dice/src/App.jsx
+++ react/react-with-local-storage/react-with-local-storage_dice/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import { D6Button } from "./components/D6Button";
 import { getD6Roll } from "./utils";
 import History from "./components/History";
 import "./App.css";
 
 export default function App() {
-  const [rolls, setRolls] = useState([]);
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
 
   const handleRoll = () => {
     setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
