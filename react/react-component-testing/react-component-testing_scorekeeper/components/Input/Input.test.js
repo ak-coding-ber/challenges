cc68acbc939700e7f839cab654f6176e7913dc0b
ak-coding-ber <a.kupka1@gmx.net>
2024-12-03 11:13:39 +0100

    wrote test #1 for Input component

--- react/react-component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react/react-component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,39 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  const handleChange = jest.fn();
+
+  render(
+    <Input
+      labelText={"input-text"}
+      placeholder={"Please type in something."}
+      name={"input"}
+      required={true}
+      onChange={handleChange}
+      value={"test value"}
+    />
+  );
+
+  // getByText addresses the label itself, not the input field
+  const label = screen.getByText(/input-text/i);
+  const inputField = screen.getByRole("textbox");
+
+  // label checks
+  expect(label).toBeInTheDocument();
+  expect(label).toHaveAttribute("for", "input");
+  expect(label).toHaveTextContent("input-text");
+
+  // input field checks
+  expect(inputField).toBeInTheDocument();
+  expect(inputField).toHaveAttribute(
+    "placeholder",
+    "Please type in something."
+  );
+  expect(inputField).toHaveAttribute("value", "test value");
+  expect(inputField).toHaveAttribute("name", "input");
+
+  expect(inputField).toBeRequired();
+});
 
 test("calls callback on every user input", async () => {});
