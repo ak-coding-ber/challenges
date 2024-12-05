14d9275bbd3a588f5a796487c8d1248d8d8b8443
ak-coding-ber <a.kupka1@gmx.net>
2024-12-05 12:47:22 +0100

    wrote tests #1 & #2 for GameForm component

--- react/react-component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
+++ react/react-component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
@@ -8,9 +8,25 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders two input fields and a button", () => {});
+test("renders two input fields and a button", () => {
+  render(<GameForm />);
 
-test("renders a form with the accessible name 'Create a new game'", () => {});
+  // 2 input fields should be rendered
+  const InputFields = screen.getAllByRole("textbox");
+  expect(InputFields).toHaveLength(2);
+
+  //a button should be rendered - extra Check: button text should be 'Create game'
+  const createGameButton = screen.getByRole("button", { name: "Create game" });
+  expect(createGameButton).toBeInTheDocument();
+});
+
+test("renders a form with the accessible name 'Create a new game'", () => {
+  render(<GameForm />);
+
+  const form = screen.getByRole("form", { name: "Create a new game" });
+
+  expect(form).toBeInTheDocument();
+});
 
 test("submits the correct form data when every field is filled out", async () => {});
 
