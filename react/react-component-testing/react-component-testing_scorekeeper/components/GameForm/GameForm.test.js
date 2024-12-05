357994f3c0395453fcf049fb3b537140cc2ec3cb
ak-coding-ber <a.kupka1@gmx.net>
2024-12-05 15:15:15 +0100

    wrote test #3 for GameForm component

--- react/react-component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
+++ react/react-component-testing/react-component-testing_scorekeeper/components/GameForm/GameForm.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import GameForm from "./index";
+import { handleClientScriptLoad } from "next/script";
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -28,6 +29,30 @@ test("renders a form with the accessible name 'Create a new game'", () => {
   expect(form).toBeInTheDocument();
 });
 
-test("submits the correct form data when every field is filled out", async () => {});
+test("submits the correct form data when every field is filled out", async () => {
+  const handleSubmit = jest.fn();
+
+  render(<GameForm onCreateGame={handleSubmit} />);
+
+  const user = userEvent.setup();
+
+  // simulate filling out the form fields
+  const [nameOfGame, playerNames] = screen.getAllByRole("textbox");
+  await user.type(nameOfGame, "Gothic 2");
+  await user.type(playerNames, "Skip, Ronald");
+
+  // simulate pressing the submit button
+  const submitButton = screen.getByRole("button", { name: "Create game" });
+  await user.click(submitButton);
+
+  // checking if the mock function was called with the correct data
+  expect(handleSubmit).toHaveBeenCalledWith({
+    nameOfGame: "Gothic 2",
+    playerNames: ["Skip", "Ronald"],
+  });
+
+  // Submit function should have been called only one time
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+});
 
 test("does not submit form if one input field is left empty", async () => {});
